edf458f7a3e4394d18a9d3cb3bf09253a835f563
Alejandro Gallo <aamsgallo@gmail.com>
2017-12-03 22:21:52 +0100

    add script

---
+++ tools/goslate-translate.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import yaml
+import sys
+import os
+import goslate
+
+gs = goslate.Goslate()
+
+language = sys.argv[1]
+lang_code = sys.argv[2]
+
+print(language, lang_code)
+
+out = "new.yaml"
+
+with open("data/english/verbs/data.yaml") as fd:
+    english_yaml = yaml.load(fd)
+
+other_yaml = [d for d in english_yaml]
+
+for d in other_yaml:
+    del d["root"]
+    time.sleep(1)
+    d["infinitive"] = gs.translate(d["infinitive"], lang_code)
+    print(d)
+
+
+with open(out, 'w+') as fd:
+    yaml.dump(other_yaml, fd)
+
+
+#vim-run: python3 % hebrew he
